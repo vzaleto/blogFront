ee4b8ca4073c3928076c0fd813c1b103b8a6e337
vzaleto <vzaleto@gmail.com>
2026-05-08 19:10:36 +0300

    button delete

--- src/components/postDelete/PostDelete.tsx
+++ src/components/postDelete/PostDelete.tsx
@@ -1,7 +1,7 @@
-
 import {fetchPostDelete} from "../../features/postSlise/postSlice.ts";
 import {useDispatch} from "react-redux";
 import {AppDispatch} from "../../store/store.ts";
+import {fetchTags} from "../../features/tagSlice/tagSlice.ts";
 
 interface PostDeleteProps {
     elemId: number | undefined;
@@ -10,15 +10,19 @@ interface PostDeleteProps {
 export const PostDelete: React.FC<PostDeleteProps> = ({elemId}) => {
     const dispatch: AppDispatch = useDispatch();
 
-    const handleDelete = () => {
-        if(elemId){
-            dispatch(fetchPostDelete(Number(elemId)))
+    const handleDelete = async () => {
+        if (elemId) {
+            await dispatch(fetchPostDelete(Number(elemId)))
+            dispatch(fetchTags())
         }
     }
 
     return (
         <div>
-            <button type="button" className="w-28 py-2 text-center rounded-3xl text-base block border-amber-700 border-2 text-red-900  hover:border-rose-400 transition" onClick={handleDelete} > delete</button>
+            <button type="button"
+                    className="w-28 py-2 text-center rounded-3xl text-base block border-amber-700 border-2 text-red-900  hover:border-rose-400 transition"
+                    onClick={handleDelete}> delete
+            </button>
         </div>
     )
 }
