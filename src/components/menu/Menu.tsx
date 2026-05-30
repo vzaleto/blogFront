a0a62dd94142c027f5f2a88f59c0aa4027f61aff
vzaleto <vzaleto@gmail.com>
2026-05-30 20:03:00 +0300

    edit/delete

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -4,19 +4,19 @@ export const Menu = () => {
     return (
         <div className='flex gap-4 menu-link'>
             <div className="btn-wrap">
-                <Link className="hover-cut" data-text="sport" to="">sport </Link>
+                <Link className="hover-cut" data-text="sport" to="">sports</Link>
             </div>
             <div className="btn-wrap">
-                <Link className="hover-cut" data-text="technology" to="">technology</Link>
+                <Link className="hover-cut" data-text="science" to="">science</Link>
             </div>
             <div className="btn-wrap">
                 <Link className="hover-cut" data-text="food" to="">food</Link>
             </div>
             <div className="btn-wrap">
-                <Link className="hover-cut" data-text="love" to="">love</Link>
+                <Link className="hover-cut" data-text="health" to="">health</Link>
             </div>
             <div className="btn-wrap">
-                <Link className="hover-cut" data-text="sheet" to="">sheet</Link>
+                <Link className="hover-cut" data-text="education" to="">education</Link>
             </div>
             <div className="btn-wrap">
                 <Link className="hover-cut" data-text="politics" to="">politics</Link>
