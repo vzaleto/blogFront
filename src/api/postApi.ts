8920019180dec885e28e82fd505bf51f4567518e
vzaleto <vzaleto@gmail.com>
2026-06-04 19:18:56 +0300

    paper style

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosHeaders} from "axios";
-//const API_URL = 'http://localhost:3000/api';
-        const API_URL = `${import.meta.env.VITE_API_URL}/api`;
+// const API_URL = 'http://localhost:3000/api';
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 export const apiClient = axios.create({
     baseURL:`${API_URL}`,
